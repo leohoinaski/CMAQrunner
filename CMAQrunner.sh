db07a5e77c095262183dabe87ab92ea69824f1de
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2022-09-27 14:39:23 -0300

    runningWmercator

--- CMAQrunner.sh
+++ CMAQrunner.sh
@@ -39,14 +39,14 @@ CMAQ_HOME=/home/lcqar/CMAQ_REPO
 GDNAM=BR_2019
 STARTDAY=2019-01-01
 #NDAYS=2 not working yet
-ncols=229
-nrows=219
-wrf_dir=/home/WRFout/share/BR_222x191_SC_232x186/BR
+ncols=219
+nrows=229
+wrf_dir=/home/WRFout/share/BR_SC_testes/teste/BR
 wrf_file=${wrf_dir}/wrfout_d01_2018-12-31_18:00:00
 wrfDomain=1 # d01 = 1 and d02 = 2
 NPCOL=7 # Define number of processors to use - NPCOL*NPROW
 NPROW=5
-NLAYS=20
+NLAYS=33
 NEW_START=TRUE
 
 #==========================================PROCESSING==================================
@@ -105,14 +105,14 @@ do
   # echo ${CMAQ_HOME} ${wrf_dir} ${GDNAM} ${YYYYMMDD} ${YYYYMMDDend} ${NPCOL} ${NPROW} ${NLAYS} ${NEW_START} ${YYYYMMDDi} ${YESTERDAY} ${wrfDomain} 
   python3 ${CDIR}/shRunnerCMAQ.py ${CMAQ_HOME} ${wrf_dir} ${GDNAM} ${YYYYMMDD} ${YYYYMMDDend} ${NPCOL} ${NPROW} ${NLAYS} ${NEW_START} ${YYYYMMDDi} ${YESTERDAY} ${wrfDomain}
   echo '------------------------Running MCIP------------------------'
-  cd  ${CMAQ_HOME}/PREP/mcip/scripts && ./run_mcip.csh >&! mcip.log; cd -
+  cd  ${CMAQ_HOME}/PREP/mcip/scripts && ./run_mcip.csh #>&! mcip.log; cd -
 
   if test ${day} -lt 1 ;then
-    runOrnotRoadDens=0 # 0 if you already have the roadDensity files
-    runOrnotRoadEmiss=0 # 0 if you already have the roadEmiss files
-    runOrnotMergeRoadEmiss=0 # 0 if you already have the mergeRoadEmiss files
-    runOrnotBRAVES2netCDF=0 # 0 if you already have the annual netCDF files from BRAVES
-    runOrnotCMAQemiss=1
+    #runOrnotRoadDens=1 # 0 if you already have the roadDensity files
+    #runOrnotRoadEmiss=1 # 0 if you already have the roadEmiss files
+    #runOrnotMergeRoadEmiss=1 # 0 if you already have the mergeRoadEmiss files
+    #runOrnotBRAVES2netCDF=1 # 0 if you already have the annual netCDF files from BRAVES
+    #runOrnotCMAQemiss=1
 
     echo '---------------------Running PREPMEGAN--------------------'
     #if [ ! -f "${wrf_dir}/wrfout" ]; then
@@ -120,12 +120,12 @@ do
     #fi
 
     #echo 'Making a GRIDDESC copy in MEGAN21/MEGANv2.10/work folder '
-    #cp -r ${mcipPath}/GRIDDESC ${MEGANHome}/MEGANv2.10/work/GRIDDESC
+    #cp -r ${mcipPath}/GRIDDESC ${MEGANHome}/MEGANv3.21/work/GRIDDESC
 
     python3 ${CDIR}/shRunnerMEGANv3.21.py ${MEGANHome} ${mcipPath} ${wrf_file} ${GDNAM} ${YYYY} ${STJD} ${EDJD} ${ncols} ${nrows}
     cd ${MEGANHome}/MEGAN31_Prep_Code_July2020 && ./run_prepmegan4cmaq.csh >&! prepmegan.log; cd -
     echo 'Running txt2ioapi'${pwd}
-    cd ${MEGANHome}/MEGANv3.21/work && ./run.txt2ioapi.v32.csh #>&! txtioapi.log; cd -
+    cd ${MEGANHome}/MEGANv3.21/work && ./run.txt2ioapi.v32.csh >&! txtioapi.log; cd -
     echo 'Fixing txt2ioapi files'
     #python3 ${CDIR}/MEGAN2_fixLAI_netCDF.py ${MEGANHome} ${GDNAM} 
     #mv ${MEGANHome}/inputs/LAIS46.$GDNAM.fixed.ncf ${MEGANHome}/inputs/LAIS46.$GDNAM.ncf
@@ -146,17 +146,17 @@ do
 
   echo '---------------------Running BRAVES_database-----------------'
   # Check shRunnerBRAVES_database.py for more input configurations
-  #echo ${BRAVEShome} ${mcipPath} ${GDNAM} ${YYYY} ${runOrnotRoadDens} ${runOrnotRoadEmiss} ${runOrnotMergeRoadEmiss} ${runOrnotBRAVES2netCDF} ${runOrnotCMAQemiss}
-  python3 ${CDIR}/shRunnerBRAVES_database.py ${BRAVEShome} ${mcipPath} ${GDNAM} ${YYYY} ${runOrnotRoadDens} ${runOrnotRoadEmiss} ${runOrnotMergeRoadEmiss} ${runOrnotBRAVES2netCDF} ${runOrnotCMAQemiss} &
+  echo ${BRAVEShome} ${mcipPath} ${GDNAM} ${YYYY} ${runOrnotRoadDens} ${runOrnotRoadEmiss} ${runOrnotMergeRoadEmiss} ${runOrnotBRAVES2netCDF} ${runOrnotCMAQemiss}
+  #python3 ${CDIR}/shRunnerBRAVES_database.py ${BRAVEShome} ${mcipPath} ${GDNAM} ${YYYY} ${runOrnotRoadDens} ${runOrnotRoadEmiss} ${runOrnotMergeRoadEmiss} ${runOrnotBRAVES2netCDF} ${runOrnotCMAQemiss} &
 
   echo '# --------------------Running IND_Inventory------------------'
-  python3 ${CDIR}/shRunnerIND_inventory.py ${INDinventoryPath} ${mcipPath} ${GDNAM}  &
+  #python3 ${CDIR}/shRunnerIND_inventory.py ${INDinventoryPath} ${mcipPath} ${GDNAM}  &
   
   wait
   echo '------------------------Running finn2cmaq--------------------'
-  cd ${finn2cmaqPath} && ./scripts/get_nrt.py ${YYYYMMDD}; cd -
-  cd ${finn2cmaqPath} && ./scripts/txt2daily.py ${mcipPath}/GRIDDESC ${GDNAM} ${YYYY} ${finn2cmaqPath}/www.acom.ucar.edu/Data/fire/data/finn1/FINNv1.5_2019.GEOSCHEM.tar.gz ${finn2cmaqPath}/daily/${YYYY}/FINNv1.5_${YYYYMMDD}.GEOSCHEM.NRT.${GDNAM}.nc; cd -
-  cd ${finn2cmaqPath} && ./scripts/daily2hourly3d.py -d ${YYYYMMDD} ${finn2cmaqPath}/daily/${YYYY}/FINNv1.5_${YYYYMMDD}.GEOSCHEM.NRT.${GDNAM}.nc ${finn2cmaqPath}/hourly/${YYYY}/${MM}/FINNv1.5_2016.CB6r3.NRT.${GDNAM}.3D.${YYYY}-${MM}-${DD}.nc; cd -  & 
+  #cd ${finn2cmaqPath} && ./scripts/get_nrt.py ${YYYYMMDD}; cd -
+  #cd ${finn2cmaqPath} && ./scripts/txt2daily.py ${mcipPath}/GRIDDESC ${GDNAM} ${YYYY} ${finn2cmaqPath}/www.acom.ucar.edu/Data/fire/data/finn1/FINNv1.5_2019.GEOSCHEM.tar.gz ${finn2cmaqPath}/daily/${YYYY}/FINNv1.5_${YYYYMMDD}.GEOSCHEM.NRT.${GDNAM}.nc; cd -
+  #cd ${finn2cmaqPath} && ./scripts/daily2hourly3d.py -d ${YYYYMMDD} ${finn2cmaqPath}/daily/${YYYY}/FINNv1.5_${YYYYMMDD}.GEOSCHEM.NRT.${GDNAM}.nc ${finn2cmaqPath}/hourly/${YYYY}/${MM}/FINNv1.5_2016.CB6r3.NRT.${GDNAM}.3D.${YYYY}-${MM}-${DD}.nc; cd -  & 
 
   echo '-------------------------Running MEGAN-----------------------'
   # Check shRunnerMEGAN.py for more input configurations
@@ -168,7 +168,7 @@ do
   cd ${MEGANHome}/MEGANv3.21/work && ./run.megcan.v32.csh #>&! met2mgn.log; cd -
   cd ${MEGANHome}/MEGANv3.21/work && ./run.megsea.v32.csh #>&! met2mgn.log; cd -
   cd ${MEGANHome}/MEGANv3.21/work && ./run.megvea.v32.csh #>&! met2mgn.log; cd -
-  cd ${MEGANHome}/MEGANv3.21/work && ./run.mgn2mech.v232.csh #>&! mgn2mech.log; cd -
+  cd ${MEGANHome}/MEGANv3.21/work && ./run.mgn2mech.v32.csh #>&! mgn2mech.log; cd -
   #ncatted -O -h -a NVARS,global,m,d,24 ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf
   #ncatted -O -h -a VAR-LIST,global,m,c,"ISOP            TERP            PAR             XYL             OLE             NR              MEOH            CH4             NH3             NO              ALD2            ETOH            FORM            ALDX            TOL             IOLE            CO              ETHA            ETH             ETHY            PRPA            BENZ            ACET            KET             " ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf
   #ncks -O -x -v GDAY ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf ${MEGANHome}/MEGANv2.10/outputs/MEGANv2.10.${GDNAM}.CB6.${YYYYJJJ}.ncf
